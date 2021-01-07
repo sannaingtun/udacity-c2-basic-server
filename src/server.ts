a2e5cf6dcf551f5ac459f2e1f64ae4191e74410c
San Naing Tun <sannaingtun@gmail.com>
2021-01-07 23:36:30 +0900

    feat:Add code for @TODO @TODO Add an endpoint to get a list of cars Add GET request with "/cars/" endpoint

--- src/server.ts
+++ src/server.ts
@@ -4,72 +4,87 @@ import bodyParser from 'body-parser';
 import { Car, cars as cars_list } from './cars';
 
 (async () => {
-  let cars:Car[]  = cars_list;
+  let cars: Car[] = cars_list;
 
   //Create an express applicaiton
-  const app = express(); 
+  const app = express();
   //default port to listen
-  const port = 8082; 
-  
+  const port = 8082;
+
   //use middleware so post bodies 
   //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+  app.use(bodyParser.json());
 
   // Root URI call
-  app.get( "/", ( req: Request, res: Response ) => {
+  app.get("/", (req: Request, res: Response) => {
     res.status(200).send("Welcome to the Cloud!");
-  } );
+  });
 
   // Get a greeting to a specific person 
   // to demonstrate routing parameters
   // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
-    ( req: Request, res: Response ) => {
+  app.get("/persons/:name",
+    (req: Request, res: Response) => {
       let { name } = req.params;
 
-      if ( !name ) {
+      if (!name) {
         return res.status(400)
-                  .send(`name is required`);
+          .send(`name is required`);
       }
 
       return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+        .send(`Welcome to the Cloud, ${name}!`);
+    });
 
   // Get a greeting to a specific person to demonstrate req.query
   // > try it {{host}}/persons?name=the_name
-  app.get( "/persons/", ( req: Request, res: Response ) => {
+  app.get("/persons/", (req: Request, res: Response) => {
     let { name } = req.query;
 
-    if ( !name ) {
+    if (!name) {
       return res.status(400)
-                .send(`name is required`);
+        .send(`name is required`);
     }
 
     return res.status(200)
-              .send(`Welcome to the Cloud, ${name}!`);
-  } );
+      .send(`Welcome to the Cloud, ${name}!`);
+  });
 
   // Post a greeting to a specific person
   // to demonstrate req.body
   // > try it by posting {"name": "the_name" } as 
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
-    async ( req: Request, res: Response ) => {
+  app.post("/persons",
+    async (req: Request, res: Response) => {
 
       const { name } = req.body;
 
-      if ( !name ) {
+      if (!name) {
         return res.status(400)
-                  .send(`name is required`);
+          .send(`name is required`);
       }
 
       return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+        .send(`Welcome to the Cloud, ${name}!`);
+    });
 
-  // @TODO Add an endpoint to GET a list of cars
+  // @TODO Add an endpoint to get a list of cars
   // it should be filterable by make with a query paramater
+  app.get("/cars/", (req: Request, res: Response) => {
+    // destruct our query paramaters
+    let { make } = req.query;
+
+
+    let cars_list = cars;
+
+    // if we have an optional query paramater, filter by it
+    if (make) {
+      cars_list = cars.filter((car) => car.make === make);
+    }
+
+    // return the resulting list along with 200 success
+    res.status(200).send(cars_list);
+  });
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
@@ -79,8 +94,8 @@ import { Car, cars as cars_list } from './cars';
   // it should require id, type, model, and cost
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
